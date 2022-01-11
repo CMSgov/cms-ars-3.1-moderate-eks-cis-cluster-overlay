d6599f67dcf300ceca67db68d9cf8d88e6b9ef85
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-01-11 13:03:21 -0500

    Update cms-ars-3.1-moderate-overlay.rb

--- controls/cms-ars-3.1-moderate-overlay.rb
+++ controls/cms-ars-3.1-moderate-overlay.rb
@@ -2,5 +2,14 @@
 
 include_controls "eks-cis-cluster-baseline" do
   
-  
+  control 'eks-cis-5.1.2' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (AC-3 (3)) is not included in CMS ARS 3.1'
+  end
+
+  control 'eks-cis-5.4.1' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (AC-3 (3)) is not included in CMS ARS 3.1'
+  end
+ 
 end
